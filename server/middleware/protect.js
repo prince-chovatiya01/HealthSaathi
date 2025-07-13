eef9b208738eda8aa55cafff61b0ebf5318d9d00
prince-chovatiya01 <162122655+prince-chovatiya01@users.noreply.github.com>
2025-07-13 16:56:01 +0530

    Update protect.js

--- server/middleware/protect.js
+++ server/middleware/protect.js
@@ -1,28 +1,3 @@
-// import jwt from 'jsonwebtoken';
-
-// const auth = (req, res, next) => {
-//   try {
-//     const token = req.header('Authorization')?.replace('Bearer ', '');
-    
-//     if (!token) {
-//       return res.status(401).json({ message: 'No token provided' });
-//     }
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = decoded;
-//     next();
-//   } catch (error) {
-//     res.status(401).json({ message: 'Invalid token' });
-//   }
-// };
-
-// export default auth;
-
-
-// server/middleware/authMiddleware.js
-
-// server/middleware/protect.js
-
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
